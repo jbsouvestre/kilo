33ec9964b861f5921880282c44c9abc5d6c5293d
souvestre <souvestre@avast.com>
2017-04-06 18:14:51 +0200

    comments

--- kilo.c
+++ kilo.c
@@ -6,24 +6,32 @@
 
 struct termios orig_termios;
 
-void disableRawMode() {
+void disable_row_mode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode() {
+void enable_raw_mode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
+  // called when program exits
+  atexit(disable_row_mode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+  // input flags
+  raw.c_iflag &= ~(IXON);
+
+  // ECHO: all keys are printed into the terminal
+  // ICANON: turn off canonical mode (read inpyt byte byte byte instead of line by line)
+  // c_lflag: dumping group for other states
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-  enableRawMode();
+  enable_raw_mode();
   char c;
 
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (read(STDIN_FILENO, &c, 10) > 0 && c != 'q') {
     if (isprint(c)) {
       printf("%d ('%c')\n", c, c);
     } else {
