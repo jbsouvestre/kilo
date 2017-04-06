d6fb1dc687614b9f78b400d0c0619ba19e50df4f
souvestre <souvestre@avast.com>
2017-04-06 18:30:48 +0200

    raw mode done

--- kilo.c
+++ kilo.c
@@ -1,17 +1,31 @@
+// === INCLUDES === //
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 struct termios orig_termios;
 
+// === TERMINAL === //
+
+// error handling;
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disable_row_mode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
 }
 
 void enable_raw_mode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
   // called when program exits
   atexit(disable_row_mode);
 
@@ -36,15 +50,21 @@ void enable_raw_mode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
 }
+
+// === INIT === //
 
 int main() {
   enable_raw_mode();
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if( read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN ){
+      die("read");
+    }
     if (isprint(c)) {
       printf("%d ('%c')\r\n", c, c);
     } else {
