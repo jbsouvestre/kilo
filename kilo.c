21202b88fcf20f0c32b8245a2dcfcbebd21cbb7f
souvestre <souvestre@avast.com>
2017-04-06 18:24:49 +0200

    more flags, timeout for read

--- kilo.c
+++ kilo.c
@@ -18,25 +18,39 @@ void enable_raw_mode() {
   struct termios raw = orig_termios;
 
   // input flags
-  raw.c_iflag &= ~(IXON);
+  // IXON: disable Ctrl+s Ctrl+q
+  // ICRNL Fix Ctrl+m
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
+  raw.c_cflag |= (CS8);
+
+  // fix output
+  raw.c_oflag &= ~(OPOST);
   // ECHO: all keys are printed into the terminal
   // ICANON: turn off canonical mode (read inpyt byte byte byte instead of line by line)
+  // ISIG:  turn off Ctrl+c Ctrl+
+  // IEXTEN: disable Ctrl+v
   // c_lflag: dumping group for other states
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
   enable_raw_mode();
-  char c;
 
-  while (read(STDIN_FILENO, &c, 10) > 0 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (isprint(c)) {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     } else {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     }
+    if(c == 'q') break;
   }
   return 0;
 }
